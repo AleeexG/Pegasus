7d5d2c52fc018a362f2c304eef0a8b794faa0381
AleeexG <alex.gorbanescu@yahoo.com>
2025-04-16 14:05:52 +0100

    There will be output in the console, when the player moves.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("gg.essential.loom") version "0.10.0.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm") version "1.9.0"
 }
 
 //Constants:
@@ -86,6 +87,8 @@ dependencies {
 
     // If you don't want to log in with your real minecraft account, remove this line
     runtimeOnly("me.djtheredstoner:DevAuth-forge-legacy:1.2.1")
+
+    shadowImpl(kotlin("stdlib-jdk8"))
 
 }
 
