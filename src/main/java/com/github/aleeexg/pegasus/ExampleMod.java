82d448de6167f8fd0f29dd1b9a0621484e44e3b4
AleeexG <alex.gorbanescu@yahoo.com>
2025-04-16 13:48:29 +0100

    There will be output in the console, when the player moves.

--- src/main/java/com/github/aleeexg/pegasus/ExampleMod.java
+++ src/main/java/com/github/aleeexg/pegasus/ExampleMod.java
@@ -1,16 +1,45 @@
 package com.github.aleeexg.pegasus;
 
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.init.Blocks;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = "pegasus", useMetadata=true)
+@Mod(modid = "pegasus", version = "1.0", useMetadata = true)
 public class ExampleMod {
+
+    private double lastX = 0;
+    private double lastY = 0;
+    private double lastZ = 0;
+    private boolean hasMovedBefore = false;
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
-		// Below is a demonstration of an access-transformed class access.
-        System.out.println("Color State: " + new GlStateManager.Color());
+        System.out.println("Pegasus mod initialized!");
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        Minecraft mc = Minecraft.getMinecraft();
+        if (mc.thePlayer == null) return;
+
+        EntityPlayerSP player = mc.thePlayer;
+        double x = player.posX;
+        double y = player.posY;
+        double z = player.posZ;
+
+        if (!hasMovedBefore || x != lastX || y != lastY || z != lastZ) {
+            System.out.println("Player moved to: x=" + x + ", y=" + y + ", z=" + z);
+            lastX = x;
+            lastY = y;
+            lastZ = z;
+            hasMovedBefore = true;
+        }
     }
 }
